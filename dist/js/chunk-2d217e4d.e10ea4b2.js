fbd4ab4f20b59d64d9fc879eb59b2f762e945b45
oksanka <drobchak.o@gmail.com>
2021-06-11 11:53:06 +0200

    add build folder

---
+++ dist/js/chunk-2d217e4d.e10ea4b2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d217e4d"],{c956:function(e,n,a){"use strict";a.r(n);var t=function(){var e=this,n=e.$createElement,a=e._self._c||n;return a("div",[e._v(" Shapes ")])},c=[],s={name:"Shapes"},u=s,d=a("2877"),p=Object(d["a"])(u,t,c,!1,null,"79adde10",null);n["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d217e4d.e10ea4b2.js.map
